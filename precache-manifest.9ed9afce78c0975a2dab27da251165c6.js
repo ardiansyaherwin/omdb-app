0aec71e33be13f51ad001d6f0e6ad44425da0704
Erwin <erwin.ardiansyah@yummycorp.com>
2020-05-31 20:45:28 +0700

    Updates

---
+++ precache-manifest.9ed9afce78c0975a2dab27da251165c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0472e7b0a83952ebdacf100015fabc4",
+    "url": "/omdb-app/index.html"
+  },
+  {
+    "revision": "eafda82e6a5dc11631dc",
+    "url": "/omdb-app/static/css/main.23cbe39f.chunk.css"
+  },
+  {
+    "revision": "c2c30f91068dd5e94dd4",
+    "url": "/omdb-app/static/js/2.f2dbc23f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/omdb-app/static/js/2.f2dbc23f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eafda82e6a5dc11631dc",
+    "url": "/omdb-app/static/js/main.8513e104.chunk.js"
+  },
+  {
+    "revision": "2f8599061314ff6b7b91",
+    "url": "/omdb-app/static/js/runtime-main.94d7a261.js"
+  }
+]);
